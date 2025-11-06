eaa170f33a9b02601fe94ff3cc32513566a31493
emgracegeorge <emgracegeorge@gmail.com>
2025-11-05 20:17:17 -0600

    clearing out JS

--- scripts/background-select.js
+++
@@ -1,6 +0,0 @@
-let element = document.querySelector("body");
-let arrImages = ["../img/desktop/boat.jpg", "../img/desktop/butterfly1.jpg", "../img/desktop/butterfly2.jpg",
-                "../img/desktop/seattle.jpg", "../img/desktop/plant.jpg"];
-let randomNumber = Math.floor(Math.random() * arrImages.length());
-
-element.style.background = "url(" + arrImages[randomNumber]+ ")";
